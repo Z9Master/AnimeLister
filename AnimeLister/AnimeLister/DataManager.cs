0c9ee57c07ced3695aef47b6db1c8c67e415e13e
Z9Master <34867938+Z9Master@users.noreply.github.com>
2020-09-08 20:27:38 +0200

    Update
    Program can show stored infomation

---
+++ AnimeLister/AnimeLister/DataManager.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
+
+namespace AnimeLister
+{
+    public class DataManager
+    {
+        // Unique id for indentify one time tasks
+        private int id = 0;
+
+        // Path to the data task file
+        string filePath = @"E:\Project\C#\AnimeLister\AnimeLister\AnimeLister\AnimeLister\Data\AnimeListData.txt";
+
+        // List to store task to save to the file
+        public List<AnimeItem> AnimeList = new List<AnimeItem>();
+
+        // The list to load file text to parse to the ListOneTimeEvents
+        public List<string> lines;
+
+        public void LoadFile()
+        {
+            try
+            {
+                lines = File.ReadAllLines(filePath).ToList();
+                AnimeList.Clear();
+                foreach (var line in lines)
+                {
+                    string[] entries = line.Split(';');
+                    AnimeItem items = new AnimeItem();
+
+                    
+                    items.animeId = Int32.Parse(entries[0]);
+                    items.animeName = entries[1];
+                    items.animeYear = Int32.Parse(entries[2]);
+                    items.animeGenre = entries[3];
+                    items.animeSeen = Int32.Parse(entries[4]);
+                    items.animePictureUrl = entries[5];
+                    items.animeNote = entries[6];
+
+                    AnimeList.Add(items);
+                } 
+            }
+            catch
+            {
+                MessageBox.Show("Can´t load data file", "Warning", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public void Seen()
+        {
+            try
+            {
+                lines = File.ReadAllLines(filePath).ToList();
+                AnimeList.Clear();
+                foreach (var line in lines)
+                {
+                    string[] entries = line.Split(';');
+                    AnimeItem items = new AnimeItem();
+
+
+                    items.animeId = Int32.Parse(entries[0]);
+                    items.animeName = entries[1];
+                    items.animeYear = Int32.Parse(entries[2]);
+                    items.animeGenre = entries[3];
+                    items.animeSeen = Int32.Parse(entries[4]);
+                    items.animePictureUrl = entries[5];
+                    items.animeNote = entries[6];
+
+                    if (items.animeSeen == 1)
+                    {
+                        AnimeList.Add(items);
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Can´t load data file", "Warning", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            
+        }
+
+        public void UnSeen()
+        {
+            try
+            {
+                lines = File.ReadAllLines(filePath).ToList();
+                AnimeList.Clear();
+                foreach (var line in lines)
+                {
+                    string[] entries = line.Split(';');
+                    AnimeItem items = new AnimeItem();
+
+
+                    items.animeId = Int32.Parse(entries[0]);
+                    items.animeName = entries[1];
+                    items.animeYear = Int32.Parse(entries[2]);
+                    items.animeGenre = entries[3];
+                    items.animeSeen = Int32.Parse(entries[4]);
+                    items.animePictureUrl = entries[5];
+                    items.animeNote = entries[6];
+
+                    if (items.animeSeen == 0)
+                    {
+                        AnimeList.Add(items);
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Can´t load data file", "Warning", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+
+        public DataManager()
+        {
+            LoadFile();
+        }
+
+    }
+
+   
+
+    public class AnimeItem : IEquatable<AnimeItem>
+    {
+        public string animeName { get; set; }
+        public int animeId { get; set; }
+        public int animeYear { get; set; }
+        public int animeSeen { get; set; }
+        public string animeGenre { get; set; }
+        public string animePictureUrl { get; set; }
+        public string animeNote { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            AnimeItem objAsPart = obj as AnimeItem;
+            if (objAsPart == null) return false;
+            else return Equals(objAsPart);
+        }
+
+        public override int GetHashCode()
+        {
+            return animeId;
+        }
+
+        public bool Equals(AnimeItem other)
+        {
+            if (other == null) return false;
+            return (this.animeId.Equals(other.animeId));
+        }
+    }
+}
--- AnimeLister/AnimeLister/MainWindow.xaml.cs
+++ AnimeLister/AnimeLister/MainWindow.xaml.cs
@@ -18,11 +18,112 @@ namespace AnimeLister
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+                //MessageBox.Show("Debug");
     public partial class MainWindow : Window
     {
+        DataManager data = new DataManager();
         public MainWindow()
         {
             InitializeComponent();
+            Setup();
         }
+
+        void Setup()
+        {
+            LB_ListAnime.ItemsSource = data.AnimeList;
+        }
+
+        #region Windows_Btn_Event
+        private void Btn_Click_Close(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void Btn_Click_Maximize(object sender, RoutedEventArgs e)
+        {
+            if(this.WindowState == System.Windows.WindowState.Maximized)
+            {
+                this.WindowState = System.Windows.WindowState.Normal;
+            }
+            else
+            {
+                this.WindowState = System.Windows.WindowState.Maximized;
+            }
+        }
+
+        private void Btn_Click_Minimize(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = System.Windows.WindowState.Minimized;
+        }
+        #endregion
+
+        #region Btn_Filter
+        private void Btn_Click_FilterAll(object sender, RoutedEventArgs e)
+        {
+            data.LoadFile();
+            LB_ListAnime.Items.Refresh();
+        }
+
+        private void Btn_Click_FilterSaw(object sender, RoutedEventArgs e)
+        {
+            data.Seen();
+            LB_ListAnime.Items.Refresh();
+        }
+
+        private void Btn_Click_FilterNew(object sender, RoutedEventArgs e)
+        {
+            data.UnSeen();
+            LB_ListAnime.Items.Refresh();
+        }
+        #endregion
+
+        #region event_LoadInformation
+        private void LB_ListAnime_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListBox LB = sender as ListBox;
+            List<AnimeItem> an = new List<AnimeItem>();
+            if(LB != null)
+            {
+                an.Add(LB.SelectedItem as AnimeItem);
+                if(an[0] != null)
+                {
+                    TBk_AnimeName.Text = an[0].animeName.ToString();
+                    TBk_AnimeYear.Text = an[0].animeYear.ToString();
+                    TBk_AnimeGenre.Text = an[0].animeGenre.ToString();
+                    TBk_AnimeNote.Text = an[0].animeNote.ToString();
+                    if (an[0].animeSeen.ToString().Equals("1"))
+                    {
+                        TBk_AnimeSeen.Text = "Saw";
+                    }
+                    else
+                    {
+                        TBk_AnimeSeen.Text = "Not seen";
+                    }
+                    if (!an[0].animePictureUrl.ToString().Equals("null"))
+                    {
+                        try
+                        {
+                            BitmapImage ImageCache = new BitmapImage();
+                            ImageCache.BeginInit();
+                            ImageCache.UriSource = new Uri(an[0].animePictureUrl.ToString());
+                            ImageCache.EndInit();
+                            Img_AnimeImage.Source = ImageCache;
+                            Img_AnimeImage.Opacity = 255;
+                        }
+                        catch
+                        {
+                            Img_AnimeImage.Opacity = 0;
+                            MessageBox.Show("Picture not found");
+                        }
+                    }
+                    else
+                    {
+                        Img_AnimeImage.Opacity = 0;
+                    }
+                }
+            }
+        }
+        #endregion
     }
 }
